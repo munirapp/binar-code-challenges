79e3c2353d52b17f9bcffcd466e51048a1e63a42
munirapp <softwaremakassar@gmail.com>
2021-01-23 13:08:56 +0800

    finish create endpoint tasks

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,15 +1,38 @@
+const db = require("../models");
+
 class TasksController {
-  get(req, res) {
-    res.send("routing untuk create tasks");
+  async get(req, res) {
+    const data = await db.tasks.findAll({
+      attributes: ["id", "users_id", "description", "status"],
+    });
+    res.json(data);
   }
-  post(req, res) {
-    res.send("routing untuk read tasks");
+  async post(req, res) {
+    const users_id = req.body["users_id"];
+    const description = req.body["description"];
+    await db.tasks.create({
+      users_id: users_id,
+      description: description,
+      status: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    res.send("success insert tasks");
   }
-  put(req, res) {
-    res.send("routing untuk update tasks");
+  async put(req, res) {
+    const description = req.body["description"];
+    const status = req.body["status"];
+    const id = req.body["id"];
+    await db.tasks.update(
+      { description: description, status: status },
+      { where: { id: id } }
+    );
+    res.send("success update tasks with id " + id);
   }
-  delete(req, res) {
-    res.send("routing untuk delete tasks");
+  async delete(req, res) {
+    const id = req.query["id"];
+    await db.tasks.destroy({ where: { id: id } });
+    res.send("success delete tasks with id " + id);
   }
 }
 
--- index.js
+++ index.js
@@ -10,4 +10,14 @@ app.use(bodyParser.json());
 
 app.use("/", routes);
 
+app.get("/api/users", async (req, res) => {
+  const fullName = req.body["full_name"];
+  await db.users.create({
+    full_name: fullName,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  res.send("sucess insert data " + fullName);
+});
+
 app.listen(5000, () => console.log("this app running on port 5000"));
